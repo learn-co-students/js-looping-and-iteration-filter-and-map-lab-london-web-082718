370143e41ce580ace144ecf21578f9d52488423f
github username <github email address>
2018-10-11 18:06:35 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution here:
+const driversWithRevenueOver = (drivers, revenue) => drivers.filter(driver => driver.revenue > revenue)
+
+const driverNamesWithRevenueOver = (drivers, revenue) => driversWithRevenueOver(drivers, revenue).map(driver => driver.name)
+
+const exactMatch = (drivers, condition) =>
+    drivers.filter(driver => {
+        let matches = false;
+
+        for (const key in condition) {
+            matches = driver[key] === condition[key];
+        }
+
+        return matches;
+    })
+
+const exactMatchToList = (drivers, condition) => exactMatch(drivers, condition).map(driver => driver.name)
+
+// const drivers = [
+//     { name: 'Sally',   revenue: 400 },
+//     { name: 'Annette', revenue: 200 },
+//     { name: 'Jim',     revenue: 150 }
+// ]
+
+// exactMatch(drivers, { name: 'Jim'})
+
+// [{ name: 'Jim',     revenue: 150 }]
+
